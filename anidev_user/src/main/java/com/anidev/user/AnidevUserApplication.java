6989d9b6c61dba1f14fb6280db36111dd753303f
sza <suzhengang9205@163.com>
2023-05-15 22:07:30 +0800

    anidev开发环境搭建

---
+++ anidev_user/src/main/java/com/anidev/user/AnidevUserApplication.java
@@ -0,0 +1,13 @@
+package com.anidev.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnidevUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnidevUserApplication.class, args);
+    }
+
+}
---
+++ anidev_user/src/test/java/com/anidev/user/AnidevUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anidev.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnidevUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
