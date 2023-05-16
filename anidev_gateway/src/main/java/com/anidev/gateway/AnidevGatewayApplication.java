babe09506ad960a1c775db7019e6416f877e7111
sza <suzhengang9205@163.com>
2023-05-16 16:41:32 +0800

    创建网关工程

---
+++ anidev_gateway/src/main/java/com/anidev/gateway/AnidevGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.anidev.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnidevGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnidevGatewayApplication.class, args);
+    }
+
+}
---
+++ anidev_gateway/src/test/java/com/anidev/gateway/AnidevGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anidev.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnidevGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
